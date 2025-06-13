9c016f55c7eaa62f23efceb842f8e2dd64bf1307
PedroHMoscardini <pedrohmoscardini@gmail.com>
2025-06-12 22:12:27 -0300

    Task: State data - unfinished

---
+++ src/main/java/Models/StateData.java
@@ -0,0 +1,46 @@
+package Models;
+
+import java.util.Map;
+
+public class StateData {
+    public static final Map<String, String[]> Estados = Map.of(
+            "Acre (AC)", new String[]{
+                    "Rio Branco",
+                    "Cruzeiro do Sul",
+                    "Sena Madureira",
+                    "Tarauacá",
+                    "Feijó",
+                    "Brasiléia",
+                    "Xapuri",
+                    "Plácido de Castro",
+                    "Rodrigues Alves",
+                    "Manoel Urbano"},
+            "Amapá (AP)\n", new String[]{
+                    "Macapá",
+                    "Santana",
+                    "Laranjal do Jari",
+                    "Oiapoque",
+                    "Mazagão",
+                    "Porto Grande",
+                    "Tartarugalzinho",
+                    "Pedra Branca do Amapari",
+                    "Ferreira Gomes",
+                    "Calçoene"},
+            "Amazonas (AM)", new String[]{
+                    "Manaus",
+                    "Parintins",
+                    "Itacoatiara",
+                    "Manacapuru",
+                    "Coari",
+                    "Tefé",
+                    "Humaitá",
+                    "Tabatinga",
+                    "Maués",
+                    "Manicoré"
+
+            }
+
+
+
+    );
+}
