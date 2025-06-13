6eb9a8bcefba91a69e0918fc05a088e41574446f
PedroHMoscardini <pedrohmoscardini@gmail.com>
2025-06-13 09:25:23 -0300

    Task: Registro de hospedagem basico

--- src/main/java/Models/Hospedagem.java
+++ src/main/java/Models/Hospedagem.java
@@ -10,12 +10,16 @@ public class Hospedagem {
     @Id
     private int id;
     private String nome;
+    private int id_estado;
+    private int id_cidade;
     private String localizacao;
     private int id_proprietario;
 
-    public Hospedagem(int id, String nome, String localizacao, int id_proprietario) {
+    public Hospedagem(int id, String nome, int id_estado , int id_cidade, String localizacao, int id_proprietario) {
         this.id = id;
         this.nome = nome;
+        this.id_estado = id_estado;
+        this.id_cidade = id_cidade;
         this.localizacao = localizacao;
         this.id_proprietario =  id_proprietario;
 
--- src/main/java/Models/StateData.java
+++ src/main/java/Models/StateData.java
@@ -1,46 +1,30 @@
 package Models;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StateData {
-    public static final Map<String, String[]> Estados = Map.of(
-            "Acre (AC)", new String[]{
-                    "Rio Branco",
-                    "Cruzeiro do Sul",
-                    "Sena Madureira",
-                    "Tarauacá",
-                    "Feijó",
-                    "Brasiléia",
-                    "Xapuri",
-                    "Plácido de Castro",
-                    "Rodrigues Alves",
-                    "Manoel Urbano"},
-            "Amapá (AP)\n", new String[]{
-                    "Macapá",
-                    "Santana",
-                    "Laranjal do Jari",
-                    "Oiapoque",
-                    "Mazagão",
-                    "Porto Grande",
-                    "Tartarugalzinho",
-                    "Pedra Branca do Amapari",
-                    "Ferreira Gomes",
-                    "Calçoene"},
-            "Amazonas (AM)", new String[]{
-                    "Manaus",
-                    "Parintins",
-                    "Itacoatiara",
-                    "Manacapuru",
-                    "Coari",
-                    "Tefé",
-                    "Humaitá",
-                    "Tabatinga",
-                    "Maués",
-                    "Manicoré"
 
-            }
+    public static final HashMap<String, String[]> Estados = new HashMap<>()  static {
+        Estados.put("Acre (AC)", new String[]{
+                "Rio Branco", "Cruzeiro do Sul", "Sena Madureira", "Tarauacá",
+                "Feijó", "Brasiléia", "Xapuri", "Plácido de Castro",
+                "Rodrigues Alves", "Manoel Urbano"
+        });
+
+        Estados.put("Amapá (AP)", new String[]{
+                "Macapá", "Santana", "Laranjal do Jari", "Oiapoque",
+                "Mazagão", "Porto Grande", "Tartarugalzinho",
+                "Pedra Branca do Amapari", "Ferreira Gomes", "Calçoene"
+        });
+
+        Estados.put("Amazonas (AM)", new String[]{
+                "Manaus", "Parintins", "Itacoatiara", "Manacapuru", "Coari",
+                "Tefé", "Humaitá", "Tabatinga", "Maués", "Manicoré"
+        });
+    }
 
 
-
-    );
 }
+
+
