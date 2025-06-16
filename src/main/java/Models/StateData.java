de44bf7bd61d74e150851206db1d448eacdf2cba
PedroHMoscardini <pedrohmoscardini@gmail.com>
2025-06-16 09:32:49 -0300

    Task: Login salva em codigo

--- src/main/java/Models/StateData.java
+++ src/main/java/Models/StateData.java
@@ -1,28 +1,53 @@
 package Models;
 
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class StateData {
 
+
     public static final HashMap<String, String[]> Estados = new HashMap<>();
+
     public StateData() {
         Estados.put("Acre (AC)", new String[]{
-                "Rio Branco", "Cruzeiro do Sul", "Sena Madureira", "Tarauacá",
-                "Feijó", "Brasiléia", "Xapuri", "Plácido de Castro",
-                "Rodrigues Alves", "Manoel Urbano"
+                "Rio Branco",
+                "Cruzeiro do Sul",
+                "Sena Madureira",
+                "Tarauacá",
+                "Feijó",
+                "Brasiléia",
+                "Xapuri",
+                "Plácido de Castro",
+                "Rodrigues Alves",
+                "Manoel Urbano"
         });
 
         Estados.put("Amapá (AP)", new String[]{
-                "Macapá", "Santana", "Laranjal do Jari", "Oiapoque",
-                "Mazagão", "Porto Grande", "Tartarugalzinho",
-                "Pedra Branca do Amapari", "Ferreira Gomes", "Calçoene"
+                "Macapá",
+                "Santana",
+                "Laranjal do Jari",
+                "Oiapoque",
+                "Mazagão",
+                "Porto Grande",
+                "Tartarugalzinho",
+                "Pedra Branca do Amapari",
+                "Ferreira Gomes",
+                "Calçoene"
         });
 
         Estados.put("Amazonas (AM)", new String[]{
-                "Manaus", "Parintins", "Itacoatiara", "Manacapuru", "Coari",
-                "Tefé", "Humaitá", "Tabatinga", "Maués", "Manicoré"
+                "Manaus",
+                "Parintins",
+                "Itacoatiara",
+                "Manacapuru",
+                "Coari",
+                "Tefé",
+                "Humaitá",
+                "Tabatinga",
+                "Maués",
+                "Manicoré"
         });
+
         Estados.put("Pará (PA)", new String[]{
                 "Belém",
                 "Ananindeua",
@@ -33,9 +58,9 @@ public class StateData {
                 "Itaituba",
                 "Bragança",
                 "Barcarena",
-                "Altamira",
-
+                "Altamira"
         });
+
         Estados.put("Rondônia (RO)", new String[]{
                 "Porto Velho",
                 "Ji-Paraná",
@@ -46,10 +71,9 @@ public class StateData {
                 "Jaru",
                 "Guajará-Mirim",
                 "Pimenta Bueno",
-                "Ouro Preto do Oeste",
-
-
+                "Ouro Preto do Oeste"
         });
+
         Estados.put("Roraima (RR)", new String[]{
                 "Boa Vista",
                 "Rorainópolis",
@@ -60,11 +84,9 @@ public class StateData {
                 "Cantá",
                 "Bonfim",
                 "São Luiz",
-                "Iracema",
-
-
-
+                "Iracema"
         });
+
         Estados.put("Tocantins (TO)", new String[]{
                 "Palmas",
                 "Araguaína",
@@ -76,10 +98,8 @@ public class StateData {
                 "Dianópolis",
                 "Tocantinópolis",
                 "Araguatins"
-
-
-
         });
+
         Estados.put("Alagoas (AL)", new String[]{
                 "Maceió",
                 "Arapiraca",
@@ -91,10 +111,8 @@ public class StateData {
                 "Delmiro Gouveia",
                 "Campo Alegre",
                 "Coruripe"
-
-
-
         });
+
         Estados.put("Bahia (BA)", new String[]{
                 "Salvador",
                 "Feira de Santana",
@@ -106,10 +124,8 @@ public class StateData {
                 "Jequié",
                 "Lauro de Freitas",
                 "Barreiras"
-
-
-
         });
+
         Estados.put("Ceará (CE)", new String[]{
                 "Fortaleza",
                 "Caucaia",
@@ -121,8 +137,8 @@ public class StateData {
                 "Maranguape",
                 "Quixadá",
                 "Icó"
-
         });
+
         Estados.put("Maranhão (MA)", new String[]{
                 "São Luís",
                 "Imperatriz",
@@ -134,9 +150,8 @@ public class StateData {
                 "Barra do Corda",
                 "Açailândia",
                 "Santa Inês"
-
-
         });
+
         Estados.put("Paraíba (PB)", new String[]{
                 "João Pessoa",
                 "Campina Grande",
@@ -148,8 +163,8 @@ public class StateData {
                 "Guarabira",
                 "Cabedelo",
                 "Itabaiana"
-
         });
+
         Estados.put("Pernambuco (PE)", new String[]{
                 "Recife",
                 "Jaboatão dos Guararapes",
@@ -161,9 +176,8 @@ public class StateData {
                 "Camaragibe",
                 "Garanhuns",
                 "Igarassu"
-
-
         });
+
         Estados.put("Piauí (PI)", new String[]{
                 "Teresina",
                 "Parnaíba",
@@ -175,9 +189,8 @@ public class StateData {
                 "Oeiras",
                 "José de Freitas",
                 "Esperantina"
-
-
         });
+
         Estados.put("Rio Grande do Norte (RN)", new String[]{
                 "Natal",
                 "Mossoró",
@@ -189,9 +202,8 @@ public class StateData {
                 "Macau",
                 "Assu",
                 "Pau dos Ferros"
-
-
         });
+
         Estados.put("Sergipe (SE)", new String[]{
                 "Aracaju",
                 "Nossa Senhora do Socorro",
@@ -203,12 +215,150 @@ public class StateData {
                 "Simão Dias",
                 "Propriá",
                 "Capela"
-
-
         });
+
+        Estados.put("Distrito Federal (DF)", new String[]{
+                "Brasília",
+                "Ceilândia",
+                "Taguatinga",
+                "Samambaia",
+                "Gama",
+                "Planaltina",
+                "Sobradinho",
+                "Recanto das Emas",
+                "Santa Maria",
+                "São Sebastião"
+        });
+
+        Estados.put("Goiás (GO)", new String[]{
+                "Goiânia",
+                "Aparecida de Goiânia",
+                "Anápolis",
+                "Rio Verde",
+                "Luziânia",
+                "Águas Lindas de Goiás",
+                "Valparaíso de Goiás",
+                "Trindade",
+                "Catalão",
+                "Formosa"
+        });
+
+        Estados.put("Mato Grosso (MT)", new String[]{
+                "Cuiabá",
+                "Várzea Grande",
+                "Rondonópolis",
+                "Sinop",
+                "Tangará da Serra",
+                "Cáceres",
+                "Primavera do Leste",
+                "Sorriso",
+                "Lucas do Rio Verde",
+                "Barra do Garças"
+        });
+
+        Estados.put("Mato Grosso do Sul (MS)", new String[]{
+                "Campo Grande",
+                "Dourados",
+                "Três Lagoas",
+                "Corumbá",
+                "Ponta Porã",
+                "Naviraí",
+                "Paranaíba",
+                "Nova Andradina",
+                "Aquidauana",
+                "Sidrolândia"
+        });
+
+        Estados.put("Espírito Santo (ES)", new String[]{
+                "Vitória",
+                "Vila Velha",
+                "Serra",
+                "Cariacica",
+                "Linhares",
+                "Cachoeiro de Itapemirim",
+                "Colatina",
+                "Guarapari",
+                "Aracruz",
+                "São Mateus"
+        });
+
+        Estados.put("Minas Gerais (MG)", new String[]{
+                "Belo Horizonte",
+                "Uberlândia",
+                "Contagem",
+                "Juiz de Fora",
+                "Betim",
+                "Montes Claros",
+                "Ribeirão das Neves",
+                "Uberaba",
+                "Governador Valadares",
+                "Ipatinga"
+        });
+
+        Estados.put("Rio de Janeiro (RJ)", new String[]{
+                "Rio de Janeiro",
+                "São Gonçalo",
+                "Duque de Caxias",
+                "Nova Iguaçu",
+                "Niterói",
+                "Belford Roxo",
+                "Campos dos Goytacazes",
+                "São João de Meriti",
+                "Petrópolis",
+                "Volta Redonda"
+        });
+
+        Estados.put("São Paulo (SP)", new String[]{
+                "São Paulo",
+                "Guarulhos",
+                "Campinas",
+                "São Bernardo do Campo",
+                "Santo André",
+                "Osasco",
+                "São José dos Campos",
+                "Ribeirão Preto",
+                "Sorocaba",
+                "Santos"
+        });
+
+        Estados.put("Paraná (PR)", new String[]{
+                "Curitiba",
+                "Londrina",
+                "Maringá",
+                "Ponta Grossa",
+                "Cascavel",
+                "São José dos Pinhais",
+                "Foz do Iguaçu",
+                "Colombo",
+                "Guarapuava",
+                "Toledo"
+        });
+
+        Estados.put("Rio Grande do Sul (RS)", new String[]{
+                "Porto Alegre",
+                "Caxias do Sul",
+                "Canoas",
+                "Pelotas",
+                "Santa Maria",
+                "Gravataí",
+                "Viamão",
+                "Novo Hamburgo",
+                "São Leopoldo",
+                "Rio Grande"
+        });
+
+        Estados.put("Santa Catarina (SC)", new String[]{
+                "Florianópolis",
+                "Joinville",
+                "Blumenau",
+                "São José",
+                "Chapecó",
+                "Itajaí",
+                "Criciúma",
+                "Jaraguá do Sul",
+                "Lages",
+                "Palhoça"
+        });
+
     }
-
-
 }
-
-
